7e6d5fb2d72d21fb57ba1faaf55f44f81cfb00e1
Atanu Paul <atanupaul2411@gmail.com>
2023-06-17 19:32:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $result = $conn->query($to_do);
     <title>To Do List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css" />
+<!--     <link rel="stylesheet" href="style.css" /> -->
 </head>
 
 <body class="mb-5 vh-100">
